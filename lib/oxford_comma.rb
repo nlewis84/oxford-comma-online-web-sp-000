45adf1c12f17b0062c0fdb59b960208dafe327f4
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-24 19:43:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   if array.count == 1
     array.join
   elsif array.count == 2
-   array.join("and")
+   array.join(" and ")
   elsif array.count >= 3
    array.last.unshift("and ")
    array.join(", ")

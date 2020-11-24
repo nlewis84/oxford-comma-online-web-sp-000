49178f985e2e333baeab5712fd6d3555e6d570f1
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-24 19:48:20 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+   array.join(" and ")
+  elsif array.count >= 3
+   array.last().prepend("and ")
+   array.join(", ")
+  end
 end
